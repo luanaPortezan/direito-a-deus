4898206e447fd877820eb631261e1caf4cd80cda
Luana Portezan <luanaportezan@gmail.com>
2024-04-21 10:21:54 -0300

    refactor: make HTML in CardBookComponent more dynamic and with directives

--- src/app/components/card-book/card-book.component.ts
+++ src/app/components/card-book/card-book.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-book',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './card-book.component.css'
 })
 export class CardBookComponent {
-
+  @Input() imageBook: string = 'https://editoraappris.com.br/wp-content/webp-express/webp-images/uploads/2023/01/9786525026824-scaled-500x714.jpg.webp';
+  @Input() imageAltBook: string = 'Capa do livro "Pessoas LGBTQIAP+ e o Direito a Deus: Minha Experiência e a Teologia Inclusiva", escrito por Keila Guedes Silva Santos';
+  @Input() descriptionBook: string = 'Nesta obra, eu o convido a desconstruir toda condenação que você aprendeu sobre pessoas LGBTQIAP+ e a refletir sobre a Teologia Inclusiva.';
+  @Input() phrase: string = 'Acredite: pessoas LGBTQIAP+ têm o direito a Deus!';
+  @Input() linkUrlBook: string = 'https://editoraappris.com.br/produto/pessoas-lgbtqiap-e-o-direito-a-deus-minha-experiencia-e-a-teologia-inclusiva/'
 }
